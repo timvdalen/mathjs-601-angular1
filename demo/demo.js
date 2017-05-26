bbcffab799eae2e5afaec6f9d829702c28906b15
Tim van Dalen <Tim@timvdalen.nl>
2017-05-26 12:09:13 +0200

    Add debugging

--- demo/demo.js
+++ demo/demo.js
@@ -10,6 +10,15 @@
 			scope: {},
 			controller: ["$scope", function ($scope) {
 				$scope.value = math.parse('2 + 2').compile().eval();
+				var m = math;
+				debugger;
+				// m.parse('2+2').compile() => error
+				var tree = math.parse('2+2');
+				debugger;
+				// tree.compile() => error
+				var exp = math.parse('2+2').eval();
+				debugger;
+				// exp => 4
 			}],
 
 			template: require('html-loader!./demo.html')
